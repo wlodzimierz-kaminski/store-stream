9d7e9716c2a6c95c49b6474992b1859cc1d4f174
Nur Duisekeyev <nur.duisekeyev@hotmail.com>
2024-02-07 21:32:01 +0600

    refactor: add the use callback hook and remove unused variable

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,7 +3,8 @@ import ProductGridItems from 'components/layout/product-grid-items';
 import LoadingDots from 'components/loading-dots';
 import { defaultSort, sorting } from 'lib/constants';
 import { getProducts } from 'lib/shopify';
-import { useEffect, useState } from 'react';
+import { Product } from 'lib/shopify/types';
+import { useCallback, useEffect, useState } from 'react';
 
 export const runtime = 'edge';
 
@@ -20,36 +21,51 @@ export default function SearchPage({
   const { sort, q: searchValue } = searchParams as { [key: string]: string };
   const { sortKey, reverse } = sorting.find((item) => item.slug === sort) || defaultSort;
 
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState<Product[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [cursor, setCursor] = useState('');
   const [page, setPage] = useState(1);
 
-  async function fetchProducts() {
+  const fetchProducts = useCallback(async () => {
     try {
       setIsLoading(true);
-      const { products: fetchedProducts, hasNextPage, endCursor } = await getProducts({ sortKey, reverse, query: searchValue, cursor });
-      setProducts(prevProducts => [...prevProducts, ...fetchedProducts]);
+      const { products: fetchedProducts, endCursor } = await getProducts({
+        sortKey,
+        reverse,
+        query: searchValue,
+        cursor
+      });
+      setProducts((prevProducts) => [...prevProducts, ...fetchedProducts]);
       setCursor(endCursor || '');
     } catch (error) {
       console.error('Error fetching products:', error);
     } finally {
       setIsLoading(false);
     }
-  }
+  }, [sortKey, reverse, searchValue, cursor]);
 
-  function handleScroll() {
-    if (!isLoading && window.innerHeight + document.documentElement.scrollTop + 100 >= document.documentElement.offsetHeight) {
-      setPage(prevPage => prevPage + 1);
+  const handleScroll = useCallback(async () => {
+    if (
+      !isLoading &&
+      window.innerHeight + document.documentElement.scrollTop + 100 >=
+        document.documentElement.offsetHeight
+    ) {
+      setPage((prevPage) => prevPage + 1);
     }
-  }
+  }, [isLoading]);
 
-  async function fetchMoreProducts() {
+  const fetchMoreProducts = useCallback(async () => {
     try {
       if (!isLoading && page > 1) {
         setIsLoading(true);
-        const { products: fetchedProducts, hasNextPage, endCursor } = await getProducts({ sortKey, reverse, query: searchValue, cursor, page });
-        setProducts(prevProducts => [...prevProducts, ...fetchedProducts]);
+        const { products: fetchedProducts, endCursor } = await getProducts({
+          sortKey,
+          reverse,
+          query: searchValue,
+          cursor,
+          page
+        });
+        setProducts((prevProducts) => [...prevProducts, ...fetchedProducts]);
         setCursor(endCursor || '');
       }
     } catch (error) {
@@ -57,20 +73,20 @@ export default function SearchPage({
     } finally {
       setIsLoading(false);
     }
-  }
+  }, [cursor, isLoading, page, reverse, searchValue, sortKey]);
 
   useEffect(() => {
     fetchProducts();
-  }, [sortKey, reverse, searchValue, cursor]);
+  }, [fetchProducts]);
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
-  }, [isLoading]);
+  }, [handleScroll]);
 
   useEffect(() => {
     fetchMoreProducts();
-  }, [page]);
+  }, [fetchMoreProducts]);
 
   const resultsText = products.length > 1 ? 'results' : 'result';
 
